082e3975dd7ac8c6f57c206effbe38e9254b9c92
Zhengkai Jiang <justinjiang1201@gmail.com>
2018-11-02 11:59:46 -0400

    initial jungle game

---
+++ Jungle/CatPiece.java
@@ -0,0 +1,30 @@
+import java.awt.Color;
+import javax.swing.Icon;
+
+public class CatPiece extends Piece {
+
+	/* create a CatPiece */
+	public CatPiece(ChessBoard board, Color color, String label, Jungle.Side side, Icon icon) {
+		super(board, color, label, side, icon);
+	}
+
+	/*
+	 * check whether the piece can move to input position while there is an empty
+	 * legal square
+	 */
+	public boolean isLegalNonCaptureMove(int x, int y) {
+		boolean result = false;
+		if (this.getSide() == Jungle.Side.NORTH) {
+			if ((x - this.getRow() == 1 && y == this.getColumn()) || (x == this.getRow() && Math.abs(y - this.getColumn()) == 1)) {
+				result = true;
+			}
+		} 
+		else {
+			if ((x - this.getRow() == -1 && y == this.getColumn()) || (x == this.getRow() && Math.abs(y - this.getColumn()) == 1)) {
+				result = true;
+			}
+		}
+		return result;
+	}
+
+}
---
+++ Jungle/WolfPiece.java
@@ -0,0 +1,29 @@
+import java.awt.Color;
+
+import javax.swing.Icon;
+
+public class WolfPiece extends Piece{
+	/* create a wolf Piece */
+	public WolfPiece(ChessBoard board, Color color, String label, Jungle.Side side, Icon icon) {
+		super(board, color, label, side, icon);
+	}
+
+	/*
+	 * check whether the piece can move to input position while there is an empty
+	 * legal square
+	 */
+	public boolean isLegalNonCaptureMove(int x, int y) {
+		boolean result = false;
+		if (this.getSide() == Jungle.Side.NORTH) {
+			if ((x - this.getRow() == 1 && y == this.getColumn()) || (x == this.getRow() && Math.abs(y - this.getColumn()) == 1)) {
+				result = true;
+			}
+		} 
+		else {
+			if ((x - this.getRow() == -1 && y == this.getColumn()) || (x == this.getRow() && Math.abs(y - this.getColumn()) == 1)) {
+				result = true;
+			}
+		}
+		return result;
+	}
+}
